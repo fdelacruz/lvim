642d503a71abef0ca5c0b6a4f19b2c540a246651
Francisco De La Cruz <helloquico@gmail.com>
2023-03-02 11:50:30 +0530

    rm: copilot

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -75,28 +75,6 @@ lvim.plugins = {
     build = "cd js && npm ci",
   },
   -- { "tzachar/cmp-tabnine", build = "./install.sh" },
-  {
-    "zbirenbaum/copilot.lua",
-    -- event = { "VimEnter" },
-    config = function()
-      vim.defer_fn(function()
-        require("copilot").setup {
-          plugin_manager_path = os.getenv "LUNARVIM_RUNTIME_DIR" .. "/site/pack/packer",
-        }
-      end, 100)
-    end,
-  },
-  {
-    "zbirenbaum/copilot-cmp",
-    after = { "copilot.lua" },
-    config = function()
-      require("copilot_cmp").setup {
-        formatters = {
-          insert_text = require("copilot_cmp.format").remove_existing,
-        },
-      }
-    end,
-  },
   -- "MunifTanjim/nui.nvim",
   -- {
   --   "folke/noice.nvim",
