b550bb71f45bc1436b8747b78ad19e10320a655b
Francisco De La Cruz <helloquico@gmail.com>
2023-03-05 05:06:20 +0530

    jump to last cursor position

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -7,6 +7,16 @@
 --   end,
 -- })
 
+vim.api.nvim_create_autocmd("BufReadPost", {
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
+
 vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
   callback = function()
     vim.cmd "set formatoptions-=cro"
