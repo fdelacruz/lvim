f7c3b146d16c336a07833507d55bd0b383112520
Francisco De La Cruz <helloquico@gmail.com>
2023-03-13 14:28:50 +0530

    update leader keymaps

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -1,10 +1,13 @@
 M = {}
-lvim.leader = "space"
 
 local opts = { noremap = true, silent = true }
 -- For the description on keymaps, I have a function getOptions(desc) which returns noremap=true, silent=true and desc=desc. Then call: keymap(mode, keymap, command, getOptions("some randome desc")
 
 local keymap = vim.keymap.set
+
+keymap("n", "<Space>", "", opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 keymap("n", "<C-Space>", "<cmd>WhichKey \\<space><cr>", opts)
 keymap("n", "<C-i>", "<C-i>", opts)
