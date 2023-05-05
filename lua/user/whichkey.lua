49e9bf839788ee16d9be51df73939deda2a806b4
Francisco De La Cruz <helloquico@gmail.com>
2023-05-05 15:49:29 +0200

    feat(harpoon): update keymaps

--- lua/user/whichkey.lua
+++ lua/user/whichkey.lua
@@ -161,7 +161,6 @@ local m_mappings = {
   m = { '<cmd>lua require("harpoon.mark").add_file()<cr>', "Harpoon" },
   ["."] = { '<cmd>lua require("harpoon.ui").nav_next()<cr>', "Harpoon Next" },
   [","] = { '<cmd>lua require("harpoon.ui").nav_prev()<cr>', "Harpoon Prev" },
-  l = { "<cmd>lua require('user.bfs').open()<cr>", "Buffers" },
   j = { "<cmd>silent BookmarkNext<cr>", "Next" },
   s = { "<cmd>Telescope harpoon marks<cr>", "Search Files" },
   k = { "<cmd>silent BookmarkPrev<cr>", "Prev" },
