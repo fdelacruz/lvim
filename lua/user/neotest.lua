42613ae58ec269c9affa17006cd2de6c6b54fb6b
Francisco De La Cruz <helloquico@gmail.com>
2023-09-07 07:55:52 +0200

    perf(neotest): use pcall()

--- lua/user/neotest.lua
+++ lua/user/neotest.lua
@@ -1,12 +1,17 @@
 local M = {}
 
 function M.setup()
-  require("neotest").setup {
+  local status_ok, neotest = pcall(require, "neotest")
+  if not status_ok then
+    return
+  end
+
+  neotest.setup {
     adapters = {
       require "neotest-python" {
         dap = {
           justMyCode = false,
-          console = "integratedTerminal"
+          console = "integratedTerminal",
         },
         args = { "--log-level", "DEBUG", "--quiet" },
         runner = "pytest",
