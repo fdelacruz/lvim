038f70bcf4dfd90a9eb8a2b6847d95467cf538ad
Francisco De La Cruz <helloquico@gmail.com>
2023-10-25 13:44:40 +0200

    feat(git-blame): update msg when not commited

--- lua/user/git.lua
+++ lua/user/git.lua
@@ -1,3 +1,4 @@
+vim.g.gitblame_message_when_not_committed = "Oh please, commit this!!"
 vim.g.gitblame_message_template = "<summary> • <date> • <author>"
 vim.g.gitblame_highlight_group = "LineNr"
 
